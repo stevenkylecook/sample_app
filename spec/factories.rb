88bc45f3d866fd9f7f7a89c62c6fe468b6ebbd3d
Steven Kyle Cook <skc2134@columbia.edu>
2012-12-15 16:45:16 -0800

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do 
 	factory :user do
-		name "Steven Kyle Cook"
-		email "stevenkcook@gmail.com"
+		sequence(:name) { |n| "Person #{n}"}
+		sequence(:email) { |n| "person_#{n}@example.com"}
 		password "mtbaker"
 		password_confirmation "mtbaker"
+
+		factory :admin do
+			admin true
+		end	
 	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,7 @@ describe User do
 
 	subject { @user }
 
+	it { should respond_to(:admin)}
 	it { should respond_to(:name)}
 	it { should respond_to(:email)}
 	it { should respond_to(:password_digest)}
@@ -29,6 +30,16 @@ describe User do
 	it { should respond_to(:authenticate)}
 
 	it { should be_valid }
+	it { should_not be_admin }
+
+	describe "with admin attribute set to 'true'" do
+		before do
+			@user.save!
+			@user.toggle!(:admin)
+		end	
+
+		it { should be_admin }
+	end	
 
 	describe "when name is not present" do
 		before { @user.name = " " }
